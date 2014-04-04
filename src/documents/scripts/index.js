718e26d3c715de7e946cfcb07e18abc833f0c6aa
Michael Williams <dinosaur@riseup.net>
2014-04-03 22:31:03 -0700

    lazy load images responsively

--- src/documents/scripts/index.js
+++ src/documents/scripts/index.js
@@ -1,1 +1,19 @@
 var Modernizr = require('./vendor/modernizr');
+
+var $ = jQuery = require('jquery');
+require('laziestloader/jquery.laziestloader');
+
+$(function() {
+  window.onresize = resize;
+  resize();
+
+  $("img.lazy").laziestloader({
+    threshold : 200,
+  }, function () {
+    this.style.opacity = 1;
+  });
+});
+
+function resize () {
+  $("img.lazy").attr("width", window.innerWidth * 0.40);
+}
